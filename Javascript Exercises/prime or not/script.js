fb18f4d9aa28d59f9c5bb2dc9960844efb0b3a80
vdrisya <vdrisya721@gmail.com>
2024-09-02 09:15:49 +0530

    Update script.js

--- Javascript Exercises/prime or not/script.js
+++ Javascript Exercises/prime or not/script.js
@@ -13,7 +13,5 @@ function checkFirstElementPrime(arr) {
     let firstElement = arr[1];
     return isPrime(firstElement) ? `${firstElement} is a prime number.` : `${firstElement} is not a prime number.`;
 }
-
-// Example usage:
 let arr = [5, 10, 15, 20];
-console.log(checkFirstElementPrime(arr));  // Output: 5 is a prime number.
+console.log(checkFirstElementPrime(arr)); 
