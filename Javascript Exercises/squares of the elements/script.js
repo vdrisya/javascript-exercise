568d2bf627a35e057b79c3e8c89553bffbda0036
vdrisya <vdrisya721@gmail.com>
2024-09-02 09:17:10 +0530

    Update script.js

--- Javascript Exercises/squares of the elements/script.js
+++ Javascript Exercises/squares of the elements/script.js
@@ -1,7 +1,5 @@
 function sumOfSquares(arr) {
     return arr.reduce((acc, num) => acc + num * num, 0);
 }
-
-// Example usage:
 let arr = [1, 2, 3, 4];
-console.log(sumOfSquares(arr));  // Output: 30
+console.log(sumOfSquares(arr)); 
